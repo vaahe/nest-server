599ab4d086a3a1b45087e471d2e381357a0425c0
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2023-07-26 17:20:27 +0400

    delete odd files

--- mongoose.config.ts
+++
@@ -1,8 +0,0 @@
-import { MongooseModuleOptions } from '@nestjs/mongoose';
-
-const USERNAME: string = process.env.USERNAME;
-const PASSWORD: string = process.env.PASSWORD;
-
-export const mongooseConfig: MongooseModuleOptions = {
-  uri: `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.yeok26c.mongodb.net/?retryWrites=true&w=majority`,
-};
