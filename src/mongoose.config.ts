62c984e5ce2b9cb6a46cf3917a39bdff9dfaa280
vahe <vahebarseghyan9999@gmail.com>
2023-08-06 21:56:07 +0400

    adding new featues

--- src/mongoose.config.ts
+++ src/mongoose.config.ts
@@ -3,8 +3,6 @@ import { MongooseModuleOptions } from '@nestjs/mongoose';
 // const USERNAME: string = process.env.USERNAME;
 // const PASSWORD: string = process.env.PASSWORD;
 
-console.log(process.env.DB_USERNAME);
-
 export const mongooseConfig: MongooseModuleOptions = {
-  uri: `link em xrgum dir stex`,
+  uri: 'stex link dir'
 };
